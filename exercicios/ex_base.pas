4327fd4ad78300f31c94abecaa39d56b4af9b925
Rodrigo Fischer <rodrigobfischer@gmail.com>
2025-09-11 17:56:52 -0300

    Added ex_base.pas

---
+++ exercicios/ex_base.pas
@@ -0,0 +1,21 @@
+{
+  Objetivo do programa: 
+  Data da criacao: YYYY-MM-DD
+  Criado por: @programacaomentoria
+}  
+program ex_base;
+	//lugar no programa onde declaro variaveis
+	var
+		// nome_variavel: tipo_variavel;
+    // exemplo: nome: string;
+    // exemplo: idade: integer;
+  const
+    // nome_constante = valor_constante;
+    // exemplo: PI = 3.14;
+
+begin
+    //lugar no programa onde escrevo as instrucoes
+    //exemplo: write('Digite seu nome:');
+    //exemplo: readln(nome);
+    //exemplo: writeln('Olá ', nome);
+end.
