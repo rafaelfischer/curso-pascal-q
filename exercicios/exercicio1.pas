e4faddcba98e90e165ec7e82505c0ffdb4441254
Rafael Fischer <rafaelfischer@gmail.com>
2025-09-12 20:38:47 -0300

    Incluso o autor da modificação

--- exercicios/exercicio1.pas
+++ exercicios/exercicio1.pas
@@ -2,8 +2,9 @@
   Objetivo do programa: escrever na tela Ola Mundo
   Data da criacao: 2025-08-28
   Criado por: @programacaomentoria
+  Alterado por: @rafaelfischer
 }  
 program exercicio1;
 begin:
-  write('Olá Mundo!'); 
+  write('Olá Mundo!');
 end.
