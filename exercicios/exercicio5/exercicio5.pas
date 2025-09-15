72ffbf0b101377b354a1f5e1fe763aa5cdb59464
Rafael Fischer <rafaelfischer@gmail.com>
2025-09-15 10:30:40 -0300

    Alteracao e compilacao do exercicio 5

--- exercicios/exercicio5/exercicio5.pas
+++ exercicios/exercicio5/exercicio5.pas
@@ -3,9 +3,20 @@
 						Como calcular: Area = lado * lado
   Data da criacao: 2025-08-28
   Criado por: @programacaomentoria
+	Ultima atualizacao: 2025-08-28
+  Alterado por: @rafaelfischer
 }  
 program exercicio5;
+var 
+	lado: real;
+	area: real;
+begin
+	writeln('Calculo da area de um quadrado');
+	writeln('-------------------------------');
 
-begin:
-
+	write('Digite o valor do lado do quadrado: ');
+	readln(lado);
+	
+	area := lado * lado;
+	writeln('A area do quadrado e: ', area:0:2);
 end.
