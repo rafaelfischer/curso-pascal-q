e998ac19bc37e7ad7ca48eb22385fc283f5c1749
Rafael Fischer <rafaelfischer@gmail.com>
2025-09-15 17:58:44 -0300

    Reorganização das pastas em grupos

--- exercicios/grupo_1/exercicio2/exercicio2.pas
+++ exercicios/grupo_1/exercicio2/exercicio2.pas
@@ -2,7 +2,7 @@
   Objetivo do programa: Perguntar o nome de quem está usando o programa e sua idade. Faça duas perguntas.
   Data da criacao: 2025-08-28
   Criado por: @programacaomentoria
-  Ultima atualizacao: 2025-08-28
+  Ultima atualizacao: 2025-09-15
   Alterado por: @rafaelfischer
 }  
 program exercicio2;
--- exercicios/grupo_1/exercicio3/exercicio3.pas
+++ exercicios/grupo_1/exercicio3/exercicio3.pas
@@ -2,7 +2,7 @@
   Objetivo do programa: Perguntar o nome de um aluno, pergunte as 4 notas e calcule a média aritmética, exibindo no final.
   Data da criacao: 2025-08-28
   Criado por: @programacaomentoria
-  Ultima atualizacao: 2025-08-28
+  Ultima atualizacao: 2025-09-15
   Alterado por: @rafaelfischer
 }  
 program exercicio3;
