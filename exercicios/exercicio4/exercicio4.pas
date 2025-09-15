45a41d0f428e86d99b079a557cee41f9acf262dd
Rafael Fischer <rafaelfischer@gmail.com>
2025-09-15 10:25:26 -0300

    Alteracao e compilacao do exercicio 4

--- exercicios/exercicio4/exercicio4.pas
+++ exercicios/exercicio4/exercicio4.pas
@@ -2,9 +2,20 @@
   Objetivo do programa: Calcule o quadrado de um número
   Data da criacao: 2025-08-28
   Criado por: @programacaomentoria
+  Ultima atualizacao: 2025-08-28
+  Alterado por: @rafaelfischer
 }  
 program exercicio4;
+var
+	numero, quadrado: real;
 
-begin:
+begin
+	writeln('Programa para calcular o quadrado de um numero');
+	writeln('----------------------------------------------');
 
+	write('Digite um numero: ');
+	readln(numero);
+
+	quadrado := numero * numero;
+	writeln('O quadrado de ', numero:0:2, ' e: ', quadrado:0:2)
 end.
