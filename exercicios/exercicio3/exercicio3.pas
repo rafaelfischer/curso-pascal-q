18d66b2b63f8a9d6fc03873974ba61e138d6601b
Rafael Fischer <rafaelfischer@gmail.com>
2025-09-15 10:17:57 -0300

    Inclusao do autor e data da alteracao

--- exercicios/exercicio3/exercicio3.pas
+++ exercicios/exercicio3/exercicio3.pas
@@ -2,6 +2,8 @@
   Objetivo do programa: Perguntar o nome de um aluno, pergunte as 4 notas e calcule a média aritmética, exibindo no final.
   Data da criacao: 2025-08-28
   Criado por: @programacaomentoria
+  Ultima atualizacao: 2025-08-28
+  Alterado por: @rafaelfischer
 }  
 program exercicio3;
 var
