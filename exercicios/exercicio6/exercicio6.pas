8d969a8a7ed5a5a011e2eaa52663f651ed29891d
Rafael Fischer <rafaelfischer@gmail.com>
2025-09-15 10:46:51 -0300

    Alteracao e compilacao do exercicio 6

--- exercicios/exercicio6/exercicio6.pas
+++ exercicios/exercicio6/exercicio6.pas
@@ -3,9 +3,21 @@
 						Como calcular: A = PI . R ao quadrado
   Data da criacao: 2025-08-28
   Criado por: @programacaomentoria
+  Ultima atualizacao: 2025-08-28
+  Alterado por: @rafaelfischer
 }  
 program exercicio6;
+const pi = 3.14159;
+var 
+	raio, area: real;
 
-begin:
+begin
+	writeln('Exercicio 6 - Calculo da area de um circulo');
+	writeln('------------------------------------------');
 
+	write('Informe o valor do raio: ');
+	readln(raio);
+	
+	area := pi * (raio * raio);
+	writeln('A area do circulo de raio ', raio:0:2, ' e: ', area:0:2);
 end.
