a33fd0b2d6ef2ad27964f76f0092dfd613a6916e
Rafael Fischer <rafaelfischer@gmail.com>
2025-09-15 10:11:47 -0300

    Organizacao dos arquivos em suas respectivas pastas

---
+++ exercicios/exercicio2/exercicio2.pas
@@ -0,0 +1,18 @@
+{
+  Objetivo do programa: Perguntar o nome de quem está usando o programa e sua idade. Faça duas perguntas.
+  Data da criacao: 2025-08-28
+  Criado por: @programacaomentoria
+  Ultima atualizacao: 2025-08-28
+  Alterado por: @rafaelfischer
+}  
+program exercicio2;
+var
+	nome: string;
+	idade: integer;
+begin
+	write('Digite seu nome: ');
+	readln(nome);
+	write('Digite sua idade: ');
+	readln(idade);
+	writeln('Ola, ', nome, '! Voce tem ', idade, ' anos.');
+end.
