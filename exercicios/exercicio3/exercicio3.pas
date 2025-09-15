eb177270b12e937e154a4c6328b846b257ad05d7
Rafael Fischer <rafaelfischer@gmail.com>
2025-09-15 10:16:36 -0300

    Alteracao e compilacao do exercicio 3

--- exercicios/exercicio3/exercicio3.pas
+++ exercicios/exercicio3/exercicio3.pas
@@ -4,7 +4,24 @@
   Criado por: @programacaomentoria
 }  
 program exercicio3;
-
-begin:
-
+var
+  nome: string;
+  nota1, nota2, nota3, nota4: real;
+  media: real;
+begin
+	writeln('Exercicio 3 - Calculo de media aritmetica');
+	writeln('-----------------------------------------');
+	write('Digite o nome do aluno: ');
+	readln(nome);
+	write('Digite a primeira nota: ');
+	readln(nota1);
+	write('Digite a segunda nota: ');
+	readln(nota2);
+	write('Digite a terceira nota: ');
+	readln(nota3);
+	write('Digite a quarta nota: ');
+	readln(nota4);
+	
+	media := (nota1 + nota2 + nota3 + nota4) / 4;
+	writeln('A media do aluno ', nome, ' e: ', media:0:2);
 end.
