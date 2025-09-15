299bed05d699f567b9c66ce35bba305be0ba3b1a
Rafael Fischer <rafaelfischer@gmail.com>
2025-09-15 10:51:50 -0300

    Alteracao e compilacao do exercicio 7

--- exercicios/exercicio7/exercicio7.pas
+++ exercicios/exercicio7/exercicio7.pas
@@ -1,11 +1,24 @@
 {
   Objetivo do programa: Calcule a área de um retângulo
-					    Area = comprimento * altura
+						Area = comprimento * altura
   Data da criacao: 2025-08-28
   Criado por: @programacaomentoria
+  Ultima atualizacao: 2025-08-28
+  Alterado por: @rafaelfischer
 }  
 program exercicio7;
+var 
+	comprimento, altura, area: real;
 
-begin:
+begin
+	writeln('Calculo da area de um retangulo');
+	writeln('-------------------------------');
 
+	write('Informe o comprimento: ');
+	readln(comprimento);
+	write('Informe a altura: ');
+	readln(altura);
+
+	area := comprimento * altura;
+	writeln('A area do retangulo e: ', area:0:2);
 end.
